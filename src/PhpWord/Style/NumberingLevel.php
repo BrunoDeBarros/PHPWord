f2c3ed97d949ddc4b0f14e0bedb98b2de2c0a77b
Bruno Moreira De Barros <bruno@terraduo.com>
2015-11-03 10:27:21 +0000

    Adds 'ideographDigital' as one of the valid numbering options.

--- src/PhpWord/Style/NumberingLevel.php
+++ src/PhpWord/Style/NumberingLevel.php
@@ -185,7 +185,7 @@ class NumberingLevel extends AbstractStyle
      */
     public function setFormat($value)
     {
-        $enum = array('bullet', 'decimal', 'upperRoman', 'lowerRoman', 'upperLetter', 'lowerLetter');
+        $enum = array('ideographDigital', 'bullet', 'decimal', 'upperRoman', 'lowerRoman', 'upperLetter', 'lowerLetter');
         $this->format = $this->setEnumVal($value, $enum, $this->format);
         return $this;
     }
